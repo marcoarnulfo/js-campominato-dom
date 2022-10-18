e7a45ce70cc3b01d5722ac40ac4ac76f1a88b6f2
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-18 15:45:56 +0200

    try to fix bug on counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,28 +11,36 @@ const nabbo = document.querySelector(".nabbo")
 const intermedio = document.querySelector(".intermedio")
 const pro = document.querySelector(".pro")
 
+const punteggioElement = document.querySelector("h2")
+
+//function gridGenerator(min, max, casellaDiffult,classColor, containerDifficult)
+
 nabbo.addEventListener("click", function () {
     containerNoob.classList.toggle("hide")
     containerInt.classList.add("hide")
     containerPro.classList.add("hide")
+    //gridGeneratorNoob(1, 100, "casella")
+    punteggioElement.classList.add("hide")  
 })
 
 intermedio.addEventListener("click", function () {
     containerInt.classList.toggle("hide")
     containerNoob.classList.add("hide")
     containerPro.classList.add("hide")
+    gridGenerator(1, 81, "casellaInt", containerInt)
+    punteggioElement.classList.add("hide") 
 })
 
 pro.addEventListener("click", function () {
     containerPro.classList.toggle("hide")
     containerNoob.classList.add("hide")
+    gridGenerator(1, 81, "casellaPro", containerPro)
     containerInt.classList.add("hide")
 })
 
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 /**
  * 
  * @param {number} min min. number
@@ -40,7 +48,7 @@ function generateRandomNumber(min, max) {
  * @returns  
  */
 
- const bombs = [] // 0 
+const bombs = [] // 0 
 
 function generateBombs(min, max) {
     while (bombs.length !== 16) { // fino a 16
@@ -55,52 +63,76 @@ function generateBombs(min, max) {
     }
 }
 
-
-
-
 generateBombs(1, 100)
-console.log(generateBombs(1, 100));
-
 console.log(bombs);
 
+counter = false
+let punteggio = 0
 
-
-
-
-
-
-for (let index = 1; index <= 100; index++) {
-    //console.log(index);
-    let casella = document.createElement("div")
-    casella.append(index)
-    containerNoob.append(casella)
-    casella.classList.add("casella")
-    // al click di una casella, aggiungo e tolgo una classe per cambiargli il colore
-    // e stampo in console il contenuto di idex (il numero della casella)
-    casella.addEventListener("click", function () {
-        if (index === bombs[0]) {
-            console.log("SI PUO' FARE!!!");
-            casella.classList.toggle("red")
-        }
-        console.log(index);
-        casella.classList.toggle("red")
-    })
+function gridGeneratorNoob(min, max, ciao) {
+    for (let index = min; index <= max; index++) {
+        //console.log(index);
+        let casella = document.createElement("div")
+        casella.append(index)
+        containerNoob.append(casella)
+        casella.classList.add(ciao)
+        // al click di una casella, aggiungo e tolgo una classe per cambiargli il colore
+        // e stampo in console il contenuto di idex (il numero della casella)
+        casella.addEventListener("click", function () {
+            if (bombs.includes(index)) {
+                console.log("SI PUO' FARE!!!");
+                counter = true
+            } else {
+                punteggio++
+                console.log(punteggio, "punteggio giocatore");
+                casella.classList.toggle("red")
+                //console.log(index);
+            }
+            if (counter) {
+                punteggioElement.innerHTML = "il tuo punteggio è: " + punteggio
+                punteggioElement.classList.remove("hide")
+                containerNoob.innerHTML = ''
+                //alert("Ups, hai preso una bomba! il tuo punteggio è: " + punteggio)
+            }
+        })
+    }
 }
 
-
-for (let index = 1; index <= 81; index++) {
-    //console.log(index);
-    let casella = document.createElement("div")
-    casella.append(index)
-    containerInt.append(casella)
-    casella.classList.add("casellaInt")
-    casella.addEventListener("click", function () {
-        console.log(index);
-        casella.classList.toggle("green")
-    })
+console.log(punteggio);
+/**
+ * 
+ * @param {number} min 
+ * @param {number} max 
+ * @param {class} casellaDiffult
+ * @param {class} classColor
+ * @param {element} containerDifficult
+ */
+function gridGenerator(min, max, casellaDiffult, containerDifficult) {
+    for (let index = min; index <= max; index++) {
+        //console.log(index);
+        let casella = document.createElement("div")
+        casella.append(index)
+        containerInt.append(casella)
+        casella.classList.add(casellaDiffult)
+        casella.addEventListener("click", function () {
+            if (bombs.includes(index)) {
+                console.log("SI PUO' FARE!!!");
+                counter = true
+            } else {
+                punteggio++
+                console.log(punteggio, "punteggio giocatore");
+                casella.classList.toggle("red")
+                //console.log(index);
+            }
+            if (counter) {
+                punteggioElement.innerHTML = "il tuo punteggio è: " + punteggio
+                punteggioElement.classList.remove("hide")
+                containerDifficult.innerHTML = ''
+                //alert("Ups, hai preso una bomba! il tuo punteggio è: " + punteggio)
+            }
+        })
+    }
 }
-
-
 
 for (let index = 1; index <= 49; index++) {
     //console.log(index);
@@ -109,37 +141,21 @@ for (let index = 1; index <= 49; index++) {
     containerPro.append(casella)
     casella.classList.add("casellaPro")
     casella.addEventListener("click", function () {
-        console.log(index);
-        casella.classList.toggle("purple")
+        if (bombs.includes(index)) {
+            console.log("SI PUO' FARE!!!");
+            counter = true
+        } else {
+            punteggio++
+            console.log(punteggio, "punteggio giocatore");
+            casella.classList.toggle("purple")
+            //console.log(index);
+        }
+        if (counter) {
+            punteggioElement.innerHTML = "il tuo punteggio è: " + punteggio
+            punteggioElement.classList.remove("hide")
+            containerPro.innerHTML = ''
+            //alert("Ups, hai preso una bomba! il tuo punteggio è: " + punteggio)
+        }
     })
 }
-
-/**
- * @param {number} n un numero intero progressivo
- * @param {number} cells_per_row max cells to generate
- * 
- */
-
-
-// devo generare 16 (numeri casuali), le bombe, i numeri non devono essere ripetuti
-/*
-const bombs = [] // 0 
-while(bombs.length !== 16) { // fino a 16
-
-    // genero un numero casuale
-    const bomb = generateRandomNumber(1, 100)
-    // verifico se il numero è stato già inserito nella lista di boms
-    // se non lo è, lo aggiungo. altrimenti vado avanti
-    if (!bombs.includes(bomb)) { 
-        // se la condizione è falsa allora eseguo il codice qui dentro
-        bombs.push(bomb)
-    }
-}
-*/
-//console.log(bombs);
-//const bombs = generateBombs(1, 100)
-//console.log(bombs);
-// funzione per generare bombe, come sopra!
-
-
 
